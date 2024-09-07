7c2383190097ff7709f81900c23981540aacb3fa
Carlossxdxd <robertomerc83@gmail.com>
2024-09-07 13:48:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Era obvio, ya sabia')
+    alert('Muak, te amo')
 });
 
 const noBtn = document.querySelector('#noBtn');
