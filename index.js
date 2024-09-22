4f5935ec39d874a3b42cf0f4bb67adb59d88c2d1
Carlossxdxd <robertomerc83@gmail.com>
2024-09-21 21:12:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Muak, te amo')
+    alert('Awwwwww GRACIASSSSSSS')
 });
 
 const noBtn = document.querySelector('#noBtn');
